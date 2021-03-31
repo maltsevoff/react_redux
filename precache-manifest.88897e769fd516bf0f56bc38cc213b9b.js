dbd78960a94860a1aefbf90406a827b282398020
Aleksandr Maltsev <alexmalczev@gmail.com>
2021-03-31 23:14:16 +0300

    Updates

---
+++ precache-manifest.88897e769fd516bf0f56bc38cc213b9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6dfe23a681481ad848010aca9048b82",
+    "url": "/react_redux/index.html"
+  },
+  {
+    "revision": "d24ed7d415a1b7d6ae33",
+    "url": "/react_redux/static/css/main.7a8e6397.chunk.css"
+  },
+  {
+    "revision": "5960e7b0e64ef3efa2b2",
+    "url": "/react_redux/static/js/2.62368e13.chunk.js"
+  },
+  {
+    "revision": "d24ed7d415a1b7d6ae33",
+    "url": "/react_redux/static/js/main.52fe13d2.chunk.js"
+  },
+  {
+    "revision": "d2d5de9e98384dc59724",
+    "url": "/react_redux/static/js/runtime~main.34373a7c.js"
+  }
+]);
